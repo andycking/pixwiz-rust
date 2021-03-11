3851f70868740e9878c5ad2111394090110f2162
Andy C King <andycking@fastmail.com>
2021-03-10 18:19:17 -0600

    Wow forgot the theme

---
+++ src/theme.rs
@@ -0,0 +1,18 @@
+use druid::Color;
+
+pub const MAIN_FILL: Color = Color::rgb8(0, 43, 54);
+
+pub const TOOLS_FILL: Color = Color::BLACK;
+pub const TOOLS_STROKE_SELECTED: Color = Color::BLACK;
+
+pub const STATUS_BAR_FILL: Color = Color::rgb8(101, 123, 131);
+pub const STATUS_BAR_STROKE: Color = Color::BLACK;
+
+pub const COLOR_WELL_STROKE: Color = Color::BLACK;
+
+pub const PALETTE_FILL: Color = Color::BLACK;
+pub const PALETTE_STROKE_SELECTED: Color = Color::BLACK;
+
+pub const CHECKERBOARD_FILL_DARK: u32 = 0x505050ff;
+pub const CHECKERBOARD_FILL_LIGHT: u32 = 0x606060ff;
+pub const CHECKERBOARD_STROKE: Color = Color::BLACK;
