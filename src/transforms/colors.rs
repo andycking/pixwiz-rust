f8c2a84084e5bed237d8508096e6de8f9241d42a
Andy C King <andycking@fastmail.com>
2021-03-21 13:59:06 -0500

    We should go row by row

--- src/transforms/colors.rs
+++ src/transforms/colors.rs
@@ -2,15 +2,15 @@ use crate::model::types::PixelHeader;
 use crate::transforms::util;
 
 pub fn grayscale(header: &PixelHeader, bytes: &mut Vec<u8>, bounds: (usize, usize, usize, usize)) {
-    for col in bounds.0..bounds.2 + 1 {
-        for row in bounds.1..bounds.3 + 1 {
-            let (r, g, b, a) = util::read(col, row, header, bytes);
+    for y in bounds.1..bounds.3 + 1 {
+        for x in bounds.0..bounds.2 + 1 {
+            let (r, g, b, a) = util::read(x, y, header, bytes);
 
             // TODO: Find nearest color in the current palette. I guess that means passing
             // in the palette too.
             let avg = ((r as u32 + g as u32 + b as u32) / 3) as u8;
 
-            util::write(col, row, header, bytes, (avg, avg, avg, a));
+            util::write(x, y, header, bytes, (avg, avg, avg, a));
         }
     }
 }
--- src/transforms/simple.rs
+++ src/transforms/simple.rs
@@ -2,9 +2,9 @@ use crate::model::types::PixelHeader;
 use crate::transforms::util;
 
 pub fn erase(header: &PixelHeader, bytes: &mut Vec<u8>, bounds: (usize, usize, usize, usize)) {
-    for col in bounds.0..bounds.2 + 1 {
-        for row in bounds.1..bounds.3 + 1 {
-            util::write(col, row, header, bytes, (0, 0, 0, 0));
+    for y in bounds.1..bounds.3 + 1 {
+        for x in bounds.0..bounds.2 + 1 {
+            util::write(x, y, header, bytes, (0, 0, 0, 0));
         }
     }
 }
