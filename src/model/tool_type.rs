3c18662bf99fd8063e47a93ba681fddb7bb654e7
Andy C King <andycking@fastmail.com>
2021-03-30 14:03:37 -0500

    This goes away now

--- src/model/tool_type.rs
+++ src/model/tool_type.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 /// Supported tool types.
 #[derive(Clone, Copy, druid::Data, Debug, PartialEq)]
 pub enum ToolType {
@@ -11,10 +9,4 @@ pub enum ToolType {
     Marquee,
     Move,
     Paint,
-}
-
-impl fmt::Display for ToolType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
 }
